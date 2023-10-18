63f1d2c771508be8d313a8e7b8b8ffe8a01d27ce
color.ui <ragipavanchary@gmail.com>
2023-10-18 07:58:25 +0000

    mock test 1A

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import MultilingualGreetings from './Components/MultilingualGreetings'
+import './App.css'
+
+const App = () => (
+  <>
+    <MultilingualGreetings />
+  </>
+)
+
+export default App
